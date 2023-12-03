82544563efd5b1fa3793b3267a3007e0760e0188
Batucagan <214.batu.cagan@tedatakent.k12.tr>
2023-12-03 14:30:19 +0300

    fix led buffer length
    change buffer length from 60 to 18

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -39,6 +39,6 @@ public final class Constants {
 
     public static class LEDConstants {
         public static final int kLEDHeader = 0;
-        public static final int kLEDBuffer = 60;
+        public static final int kLEDBuffer = 18;
     }
 }
